5330eedd21fa940d59b7b3ccdfbd6a36d8062bee
pcaliman2 <pcaliman@gmail.com>
2025-06-02 18:13:00 +0000

    Conexion_API_Prueba_01

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # load the .env file variables
 load_dotenv()
@@ -9,3 +11,4 @@ load_dotenv()
 # Get credential values
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
+
