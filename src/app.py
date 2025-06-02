ab494f39c77624e0353e118c431cbda194d3603b
pcaliman2 <pcaliman@gmail.com>
2025-06-02 18:25:39 +0000

    Conexion_API_03

--- src/app.py
+++ src/app.py
@@ -12,11 +12,15 @@ load_dotenv()
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+spotify = spotipy.Spotify(auth_manager=auth_manager)
+
 #SodaEstereo: 2TieOXUFdPe8OrB8WYgKJy
 artist_id = "2TieOXUFdPe8OrB8WYgKJy"
 
 # Get the top tracks of an artist
-results = spotify.artist_top_tracks(artist_id)
+results = spotipy.artist_top_tracks(artist_id)
 
 print(results)
 
