3545dea42dd4e4db6068b996c77a29043e60280b
pcaliman2 <pcaliman@gmail.com>
2025-06-02 15:56:24 +0000

    Ajuste_APP

--- src/app.py
+++ src/app.py
@@ -6,5 +6,6 @@ from dotenv import load_dotenv
 # load the .env file variables
 load_dotenv()
 
-
-# Spotify API credentials
+# Get credential values
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
