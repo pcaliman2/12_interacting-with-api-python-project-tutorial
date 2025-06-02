a79c9e3c795d85e5885bc98c944727350a555028
pcaliman2 <pcaliman@gmail.com>
2025-06-02 18:21:11 +0000

    Conexion_API_02

--- src/app.py
+++ src/app.py
@@ -12,3 +12,12 @@ load_dotenv()
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 
+#SodaEstereo: 2TieOXUFdPe8OrB8WYgKJy
+artist_id = "2TieOXUFdPe8OrB8WYgKJy"
+
+# Get the top tracks of an artist
+results = spotify.artist_top_tracks(artist_id)
+
+print(results)
+
+
